7b62ec9d0b4abbc8e70bcd4ea4798c9db01fc333
scene <mqiqe@163.com>
2020-06-30 00:00:17 +0800

    init

---
+++ src/main/java/com/mqiqe/wfgateway/WfgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mqiqe.wfgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mqiqe/wfgateway/WfgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqiqe.wfgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
